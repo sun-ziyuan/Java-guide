e3e0968a31f33e112d15ac9ce049cfcd8b663008
sunziyuan <sunziyuan710@gmail.com>
2024-06-05 15:44:04 +0800

    feature-设计模式-简单工厂、静态工厂

---
+++ 编程思想/01.设计模式/design-mode/factory/sinple_factory/LatteCoffee.java
@@ -0,0 +1,14 @@
+package sinple_factory;
+
+/**
+ * 拿铁咖啡
+ * @author szy
+ * @description
+ * @date 2024/6/4 17:16
+ */
+public class LatteCoffee extends Coffee {
+    @Override
+    public String getName(){
+        return "拿铁咖啡";
+    }
+}
---
+++ 编程思想/01.设计模式/design-mode/factory/static_factory/CoffeeStore.java
@@ -0,0 +1,19 @@
+package static_factory;
+
+
+/**
+ * 咖啡店
+ * @author szy
+ * @description
+ * @date 2024/6/4 17:37
+ */
+public class CoffeeStore {
+    public Coffee orderCoffee(String type) {
+        // 调用生产咖啡的方法
+        Coffee coffee = SimpleCoffeeFactory.createCoffee(type);
+        // 添加配置
+        coffee.addMilk();
+        coffee.addSugar();
+        return coffee;
+    }
+}
